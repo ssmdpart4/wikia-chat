d97a55fbcea44ab36eadb01f1f8a302a1050e17a
ssmdpart4 <ssmdpart7@gmail.com>
2017-06-06 01:02:35 -0400

    Update master.js

--- master.js
+++ master.js
@@ -1,10 +1,10 @@
-;(function(mw, $, mainRoom, toolbar){
+;(function(mw, $, mainRoom, toolbar) {
 	/**
 	 * [Object] toolbar
 	 * This object represents the configurations
 	 * used on the toolbar that will be created.
 	 **/
-	
+
 	toolbar = $.extend(toolbar, {
 		/**
 		 * [Boolean] toolbar.collapsible
@@ -22,21 +22,22 @@
 		 * [Function] toolbar.ui
 		 * This function creates the user interface for the toolbar.
 		 * @params
-		 * - type: specifies the type of a UI element
+		 * - type: specifies the type of the UI element
+		 * - id: specifies the ID of the UI element
 		 * - config: specifies the options used for the UI element (optional)
 		 **/
 		ui: function(type, id, config){
 			// This variable represents the valid types
-			var canonical_types = ['colorbox', 'combobox', 'list', 'input', 'button']
-			if (canonical_types.indexOf(type) === -1){
+			var canonical_types = ['colorbox', 'combobox', 'list', 'input', 'button'];
+			if (canonical_types.indexOf(type) === -1) {
 				throw new ReferenceError('The type that you have chosen is not valid. The valid types are ' + canonical_types.join(', ') + '.');
 			}
-			
+
 			try {
 				this.type = type;
 				this.id = id;
 				this.data = {};
-				switch (type){
+				switch (type) {
 					case 'colorbox':
 						this.color = '#000000';
 						this.element = $('<nav class="chat-ui colorbox" />');
@@ -46,7 +47,7 @@
 								['#001a00', '#002d00', '#004e00', '#006a00', '#008000', '#00af00', '#00c400', '#00ff00'],
 								['#00001a', '#00002d', '#00004e', '#00006a', '#000080', '#0000af', '#0000c4', '#0000ff']
 							];
-						if (typeof config != 'undefined'){
+						if (typeof config != 'undefined') {
 							this.color = config.color;
 							this.palette = config.palette;
 						}
@@ -65,52 +66,54 @@
 									$('<div class="colorbox-section right" />')
 										.html([
 											$('<div class="colorbox-palette-wrapper palette-wrapper palette" id="colorbox-palette" />')
-												.html(
-													Array.prototype.map.call(that.palette, function(row, index){
-														var $palette_row = $('<ul class="palette-row row" />');
-														$palette_row.addClass('row-' + index);
-														$palette_row.html(
-															Array.prototype.map.call(row, function(_color, i){
-																var $color = $('<li class="palette-color color" />');
-																$color.attr('data-color', _color);
-																$color.addClass('color-' + index + '-' + i);
-																$color.css('background-color', _color);
-																$color.on('click', function(event){
-																	var $color_large = $('#color-large'),
-																		color_value = $(event.target).attr('data-color');
-																	$('.palette-color.selected').removeClass('selected');
-																	$(event.target).addClass('selected')
-																	$color_large.css('background-color', color_value);
-																});
-																$color.addClass(function(){
-																	if ($(this).attr('data-color') == that.color){
-																		return 'selected';
-																	}
-																});
-																return $color;
-															})
+											.html(
+												Array.prototype.map.call(that.palette, function(row, index){
+													var $palette_row = $('<ul class="palette-row row" />');
+													$palette_row.addClass('row-' + index);
+													$palette_row.html(
+														Array.prototype.map.call(row, function (_color, i) {
+															var $color = $('<li class="palette-color color" />');
+															$color.attr('data-color', _color);
+															$color.addClass('color-' + index + '-' + i);
+															$color.css('background-color', _color);
+															$color.on('click', function (event) {
+																var $color_large = $('#color-large'),
+																	color_value = $(event.target).attr('data-color');
+																$('.palette-color.selected').removeClass('selected');
+																$(event.target).addClass('selected');
+																$color_large.css('background-color', color_value);
+															});
+															$color.addClass(function(){
+																if ($(this).attr('data-color') == that.color) {
+																	return 'selected';
+																}
+															});
+															return $color;
+														})
+													);
+												})
+											),
+										$('<div class="colorbox-input" id="colorbox-input" />')
+											.html([
+												$('<label for="color-input" class="colorbox-input-label" />')
+													.html('Custom Color: '),
+												$('<input type="text" id="color-input" placeholder="Enter hex here..." />')
+													.on('keypress', function(e){
+														if (!(e.which == 13 || e.keyCode == 13 || e.key == 'Enter')) return;
+														var _color = $(e.target).val();
+														if (/#([0-9a-f]{3}|[0-9a-f]{6})/.test(_color)) {
+															var $target = $('#color-large');
+															$target.css('background-color', _color);
+															$target.attr('data-color', _color);
+														} else {
+															throw new ReferenceError('The value must be a hex triplet.');
 														}
 													})
-												),
-											$('<div class="colorbox-input" id="colorbox-input" />')
-												.html([
-													$('<label for="color-input" class="colorbox-input-label" />')
-														.html('Custom Color: '),
-													$('<input type="text" id="color-input" placeholder="Enter hex here..." />')
-														.on('change keypress', function(e){
-															var _color = $(e.target).val();
-															if (/#([0-9a-f]{3}|[0-9a-f]{6})/.test(_color)){
-																var $target = $('#color-large");
-																$target.css('background-color', _color);
-																$target.attr('data-color', _color);
-															} else {
-																throw new ReferenceError('The value must be a hex triplet.');
-															}
-														})
 												])
-										])
-								]);
-								return $color_module.hide();
+											])
+										]);
+								$color_module.hide();
+								return [$color, $color_module];
 							});
 						};
 						return this;
@@ -120,6 +123,7 @@
 					case 'button':
 					default:
 						return;
+				}
 			} catch (e){
 				throw new Error(e);
 			}
