11755b54ed35a9fddac00bc650873fdac432f3bf
ssmdpart4 <ssmdpart7@gmail.com>
2017-06-11 11:57:54 -0400

    Update master.js

--- master.js
+++ master.js
@@ -1,38 +1,38 @@
 ;(function(mw, $, mainRoom, toolbar) {
-    /**
-     * [Object] toolbar
-     * This object represents the configurations
-     * used on the toolbar that will be created.
-     **/
+	/**
+	 * [Object] toolbar
+	 * This object represents the configurations
+	 * used on the toolbar that will be created.
+	 **/
 
 	toolbar = $.extend(toolbar, {
-	/**
-         * [Boolean] toolbar.collapsible
-         * This value determines whether the toolbar is collapsible.
-         * @default false
-         **/
+		/**
+		 * [Boolean] toolbar.collapsible
+		 * This value determines whether the toolbar is collapsible.
+		 * @default false
+		 **/
 		collapsible: false,
-	/**
-         * [Boolean] toolbar.collapsed
-         * This value determines whether the toolbar is collapsed by
-         * default. Will only when the toolbar is collapsible.
-	 * @default false
-         **/
+		/**
+		 * [Boolean] toolbar.collapsed
+		 * This value determines whether the toolbar is collapsed by
+		 * default. Will only when the toolbar is collapsible.
+		 * @default false
+		 **/
 		collapsed: false,
-	/**
-	 * [Object] toolbar.links
-	 * This object creates the layout for the toolbar
-	 **/
+		/**
+		 * [Object] toolbar.links
+		 * This object creates the layout for the toolbar
+		 **/
 		links: {
 			'': {}
 		},
-	/**
-	 * [Function] toolbar.showGlobalModule
-	 * This function creates a global module for the toolbar
-	 * @params
-	 * - html: represents the HTML that will go on the global module
-	 **/
-		createGlobalModule: function(html, parent, id){
+		/**
+		 * [Function] toolbar.showGlobalModule
+		 * This function creates a global module for the toolbar
+		 * @params
+		 * - html: represents the HTML that will go on the global module
+		 **/
+		createGlobalModule: function(html, parent, id) {
 			var $gm = $('<section class="global-module subnav" />');
 			if (typeof id !== undefined)
 				$gm.attr('id', id);
@@ -50,17 +50,17 @@
 			host: mw.config.get('wgServer', wgServer).replace(/http(?:s|):\/\/(.*)\.wikia\.com/, '$1')
 		},*/
 		/**
-         * [Function] toolbar.ui
-         * This function creates the user interface for the toolbar.
-         * @params
-         * - type: specifies the type of the UI element
-         * - id: specifies the ID of the UI element
-         * - config: specifies the options used for the UI element (optional)
-         **/
-		ui: function (type, id, config) {
+		 * [Function] toolbar.ui
+		 * This function creates the user interface for the toolbar.
+		 * @params
+		 * - type: specifies the type of the UI element
+		 * - id: specifies the ID of the UI element
+		 * - config: specifies the options used for the UI element (optional)
+		 **/
+		ui: function(type, id, config) {
 			// This variable represents the valid types
 			var canonical_types = ['colorbox', 'combobox', 'list', 'input', 'button', 'switch', 'tooltip'];
-			if (canonical_types.indexOf(type) === - 1) {
+			if (canonical_types.indexOf(type) === -1) {
 				throw new ReferenceError('The type that you have chosen is not valid. The valid types are ' + canonical_types.join(', ') + '.');
 			}
 
@@ -88,14 +88,14 @@
 								['#00001a', '#00002d', '#00004e', '#00006a', '#000080', '#0000af', '#0000c4', '#0000ff']
 							];
 						}
-						this.create = function () {
+						this.create = function() {
 							var $colorbox = this.element,
 								that = this;
 							$colorbox.attr('id', this.id);
-							$colorbox.html(function () {
+							$colorbox.html(function() {
 								var $color = $('<div class="colorbox-color color" />'),
 									$color_module = $('<section class="colorbox-module color-module" />');
-								$color.css('background-color', that.color).on('click', function (e_) {
+								$color.css('background-color', that.color).on('click', function(e_) {
 									$('.colorbox-module').slideToggle(500);
 								});
 								$color_module.html([
@@ -108,79 +108,79 @@
 									.html([
 										$('<div class="colorbox-palette-wrapper palette-wrapper palette" id="colorbox-palette" />')
 										.html(
-											Array.prototype.map.call(that.palette, function (row, index) {
-									var $palette_row = $('<ul class="palette-row row" />');
-									$palette_row.addClass('row-' + index);
-									$palette_row.html(
-													Array.prototype.map.call(row, function (_color, i) {
-										var $color = $('<li class="palette-color color" />');
-										$color.attr('data-color', _color);
-										$color.addClass('color-' + index + '-' + i);
-										$color.css('background-color', _color);
-										$color.on('click', function (event) {
-											var $color_large = $('#color-large'),
+											Array.prototype.map.call(that.palette, function(row, index) {
+												var $palette_row = $('<ul class="palette-row row" />');
+												$palette_row.addClass('row-' + index);
+												$palette_row.html(
+													Array.prototype.map.call(row, function(_color, i) {
+														var $color = $('<li class="palette-color color" />');
+														$color.attr('data-color', _color);
+														$color.addClass('color-' + index + '-' + i);
+														$color.css('background-color', _color);
+														$color.on('click', function(event) {
+															var $color_large = $('#color-large'),
 																color_value = $(event.target).attr('data-color');
-											$('.palette-color.selected').removeClass('selected');
-											$(event.target).addClass('selected');
-											$color_large.css('background-color', color_value);
-											$color_large.attr('data-color', color_value);
-										});
-										$color.addClass(function () {
-											if ($(this).attr('data-color') == that.color) {
-												return 'selected';
-											}
-										});
-										return $color;
-									})
+															$('.palette-color.selected').removeClass('selected');
+															$(event.target).addClass('selected');
+															$color_large.css('background-color', color_value);
+															$color_large.attr('data-color', color_value);
+														});
+														$color.addClass(function() {
+															if ($(this).attr('data-color') == that.color) {
+																return 'selected';
+															}
+														});
+														return $color;
+													})
 												);
-									return $palette_row;
-								})
+												return $palette_row;
+											})
 										),
 										$('<div class="colorbox-input" id="colorbox-input" />')
 										.html([
 											$('<label for="color-input" class="colorbox-input-label" />')
 											.html('Custom Color: '),
 											$('<input type="text" id="color-input" placeholder="Enter hex here..." />')
-											.on('keypress', function (e) {
-									if (!(e.which == 13 || e.keyCode == 13 || e.key == 'Enter')) return ;
-									var _color = $(e.target).val();
-									if (/#([0-9a-f]{3}|[0-9a-f]{6})/.test(_color)) {
-										var $target = $('#color-large');
-										$target.css('background-color', _color);
-										$target.attr('data-color', _color);
-									} else if (/([0-9a-f]{3}|[0-9a-f]{6})/.test(_color)) {
-										var $target = $('#color-large');
-										$target.css('background-color', '#' + _color);
-										$target.attr('data-color', '#' + _color);
-									} else {
-										throw new ReferenceError('The value must be a hex triplet.');
-									}
-								})
+											.on('keypress', function(e) {
+												if (!(e.which == 13 || e.keyCode == 13 || e.key == 'Enter')) return;
+												var _color = $(e.target).val();
+												if (/#([0-9a-f]{3}|[0-9a-f]{6})/.test(_color)) {
+													var $target = $('#color-large');
+													$target.css('background-color', _color);
+													$target.attr('data-color', _color);
+												} else if (/([0-9a-f]{3}|[0-9a-f]{6})/.test(_color)) {
+													var $target = $('#color-large');
+													$target.css('background-color', '#' + _color);
+													$target.attr('data-color', '#' + _color);
+												} else {
+													throw new ReferenceError('The value must be a hex triplet.');
+												}
+											})
 										])
 									]),
 									$('<div class="colorbox-submit" id="colorbox-submit" />')
-									.html(function () {
-									var UI = that.constructor,
+									.html(function() {
+										var UI = that.constructor,
 											button = new UI('button', 'colorbox-submit-button', {
-										text: 'Submit',
-										_default: true
-									});
-									button.action = function (event) {
-										var $_colorbox = $('#' + that.id),
+												text: 'Submit',
+												_default: true
+											});
+										button.action = function(event) {
+											var $_colorbox = $('#' + that.id),
 												$selected = $_colorbox.find('.color.large');
-										$_colorbox.removeClass('active');
-										$_colorbox.find('> .color').css('background-color', $selected.attr('data-color'));
-										$_colorbox.find('.color-module').hide();
-									};
-									return button.create();
-								})
+											$_colorbox.removeClass('active');
+											$_colorbox.find('> .color').css('background-color', $selected.attr('data-color'));
+											$_colorbox.find('.color-module').hide();
+										};
+										return button.create();
+									})
 								]);
 								$color_module.hide();
 								return [$color, $color_module];
 							});
 							return $colorbox;
 						};
-						this.getValue = function () {
+						this.getValue = function() {
 							var $elem = $('#' + this.id),
 								$target = $elem.find('.colorbox-color'),
 								value = $target.attr('data-color');
@@ -208,22 +208,22 @@
 							!this.grouped
 						) {
 							var t = this;
-							this.items = this.items.filter(function (item, index) {
+							this.items = this.items.filter(function(item, index) {
 								return index < t.limit;
 							});
 						}
 
-						this.create = function () {
+						this.create = function() {
 							var that = this,
 								$combobox = this.element;
 							$combobox.attr('id', this.id);
-							$combobox.html(function () {
+							$combobox.html(function() {
 								var $comboinput = $('<input type="text" class="combobox-input" />'),
 									$drop = $('<span class="combobox-drop combobox-arrow" />'),
 									$combolist = $('<div class="combobox-list-wrapper" />'),
 									$combowrapper = $('<div class="combobox-input-wrapper" />');
 								$comboinput.attr('placeholder', that.placeholder);
-								$drop.on('click', function (_e) {
+								$drop.on('click', function(_e) {
 									if ($(_e.target).parents().next('.combobox-list-wrapper').length > 0) {
 										if ($(_e.target).parents().next('.combobox-list-wrapper').is(':hidden')) {
 											$(_e.target).parents().next('.combobox-list-wrapper').slideDown(500);
@@ -234,10 +234,10 @@
 								});
 								$combowrapper.html([$comboinput, $drop]);
 								$combolist.hide();
-								$combolist.html(function () {
+								$combolist.html(function() {
 									var $html = null;
 									if (that.grouped) {
-										$html = Object.keys(that.items).map(function (name, index) {
+										$html = Object.keys(that.items).map(function(name, index) {
 											var _class = 'combobox-group-' + index,
 												_items = that.items[name],
 												$group = $('<section class="combobox-group ' + _class + '" />');
@@ -246,21 +246,21 @@
 												.html(name),
 												$('<ul class="combobox-list combobox-group-list" />')
 												.html(
-													Array.prototype.map.call(_items, function (item, i) {
-												var $item = $('<li class="combobox-item combobox-list-item" />');
-												$item.html(
+													Array.prototype.map.call(_items, function(item, i) {
+														var $item = $('<li class="combobox-item combobox-list-item" />');
+														$item.html(
 															$('<a href="#" data-target="#' + that.id + ' .combobox-input" class="combobox-item" />')
 															.html(item)
-															.on('click', function (event) {
-													event.preventDefault();
-													var input = $(event.target).attr('data-target'),
+															.on('click', function(event) {
+																event.preventDefault();
+																var input = $(event.target).attr('data-target'),
 																	$target = $(input);
-													$(event.target).parents('.combobox-list-wrapper').slideUp(500);
-													$target.val($(event.target).text());
-												})
+																$(event.target).parents('.combobox-list-wrapper').slideUp(500);
+																$target.val($(event.target).text());
+															})
 														);
-												return $item;
-											})
+														return $item;
+													})
 												)
 											]);
 											return $group;
@@ -268,21 +268,21 @@
 									} else {
 										$html = $('<ul class="combobox-list no-group" />');
 										$html.html(
-											Array.prototype.map.call(that.items, function (item, index) {
-											var $item = $('<li class="combobox-item combobox-list-item" />');
-											$item.html(
+											Array.prototype.map.call(that.items, function(item, index) {
+												var $item = $('<li class="combobox-item combobox-list-item" />');
+												$item.html(
 													$('<a href="#" data-target="#' + that.id + ' .combobox-input" class="combobox-item-link" />')
 													.text(item)
-													.on('click', function (event) {
-												event.preventDefault();
-												var input = $(event.target).attr('data-target'),
+													.on('click', function(event) {
+														event.preventDefault();
+														var input = $(event.target).attr('data-target'),
 															$target = $(input);
-												$target.val($(event.target).text());
-												$(event.target).parents('.combobox-list-wrapper').slideUp(500);
-											})
+														$target.val($(event.target).text());
+														$(event.target).parents('.combobox-list-wrapper').slideUp(500);
+													})
 												);
-											return $item;
-										})
+												return $item;
+											})
 										);
 									}
 									return $html;
@@ -292,7 +292,7 @@
 							return $combobox;
 						};
 
-						this.getValue = function () {
+						this.getValue = function() {
 							var $elem = $('#' + this.id),
 								$target = $elem.find('.combobox-input'),
 								value = $target.val();
@@ -316,12 +316,12 @@
 							this.multiple = config.multiple || true;
 						}
 
-						this.create = function () {
+						this.create = function() {
 							var list = '<ul class="dynamic-list-section">',
 								$list = this.element,
 								that = this;
 							$list.attr('id', this.id);
-							Array.prototype.forEach.call(this.items, function (item, index) {
+							Array.prototype.forEach.call(this.items, function(item, index) {
 								if (index % that.limit === 0) {
 									list += '</ul><ul class="dynamic-list-section list-section">';
 									list += '<li class="dynamic-list-item list-item item">';
@@ -339,15 +339,15 @@
 							$list.html(list);
 							return $list;
 						};
-						
-						this.getValue = function(target){
+
+						this.getValue = function(target) {
 							var value = null;
-							if (this.multiple){
+							if (this.multiple) {
 								value = [];
-								if (typeof target !== undefined){
-									target.each(function(index){
+								if (typeof target !== undefined) {
+									target.each(function(index) {
 										var $t = $(this);
-										if ($t.parents('.dynamic-list-item').hasClass('selected')){
+										if ($t.parents('.dynamic-list-item').hasClass('selected')) {
 											value[value.length] = $t.text();
 										}
 									});
@@ -362,7 +362,7 @@
 						if (typeof config !== 'undefined') {
 							this.placeholder = config.placeholder;
 						}
-						this.create = function () {
+						this.create = function() {
 							var that = this,
 								$input = this.element,
 								$input_elem = $('<input type="text" class="input-box" />');
@@ -371,7 +371,7 @@
 							$input.html($input_elem);
 							return $input;
 						};
-						this.getValue = function () {
+						this.getValue = function() {
 							var $_input = $('#' + this.id).children('input'),
 								value = $_input.val();
 							return value;
@@ -386,7 +386,7 @@
 							this.message = config.text || '';
 							this.action = config.action || null;
 						}
-						this.create = function () {
+						this.create = function() {
 							var that = this,
 								$button = $('<a class="chat-ui ui-button" />');
 							$button.attr('id', this.id);
@@ -395,7 +395,7 @@
 								$button.attr('href', this.action);
 							} else if (typeof this.action == 'function') {
 								$button.attr('href', '#');
-								$button.on('click', function (event) {
+								$button.on('click', function(event) {
 									event.preventDefault();
 									Function.prototype.apply.call(that.action, window, [event]);
 								});
@@ -414,16 +414,16 @@
 						if (typeof config !== 'undefined') {
 							this.content = config.content || null;
 							this.title = config.title || '';
-							this.notitle = typeof this.title == 'string' ? false: true;
+							this.notitle = typeof this.title == 'string' ? false : true;
 							this.inline = !config.block || true;
 						}
-						this.create = function () {
+						this.create = function() {
 							var that = this,
 								$tooltip =
-									this.inline ?
-									$('<span class="chat-ui tooltip" />') :
-							$('<div class="chat-ui tooltip block" />'),
-							$target = this.target.wrap('<div class="chat-ui tooltip-container"></div>');
+								this.inline ?
+								$('<span class="chat-ui tooltip" />') :
+								$('<div class="chat-ui tooltip block" />'),
+								$target = this.target.wrap('<div class="chat-ui tooltip-container"></div>');
 							if (this.inline) {
 								$tooltip.text(this.content);
 							} else {
@@ -432,10 +432,10 @@
 							$tooltip.hide();
 							if (!$tooltip.exists()) {
 								$target.append($tooltip);
-								$target.hover(function (event) {
+								$target.hover(function(event) {
 									var $_tooltip = $(event.target).children('.tooltip');
 									$_tooltip.fadeIn();
-								}, function (event) {
+								}, function(event) {
 									var $_tooltip = $(event.target).children('.tooltip');
 									$_tooltip.fadeOut();
 								});
@@ -445,7 +445,7 @@
 					case 'switch':
 						return this;
 					default:
-						return ;
+						return;
 				}
 			} catch (e) {
 				throw new Error(e);
